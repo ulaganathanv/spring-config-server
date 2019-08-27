5d289a1d0e541e849b33d8d7cedb1619b5d9542f
Ulaganathan Vallinayagam <ulag.india@gmail.com>
2019-08-27 15:04:10 +0530

    Base Version

---
+++ src/test/java/com/example/springconfigserver/SpringconfigserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springconfigserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringconfigserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
